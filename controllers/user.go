36c3cbf4c8344127c636fd21801d04ea81cf5354
lpg-it <lipeiguan@gmail.com>
2020-05-15 21:15:26 +0800

    增加注册 登录功能

--- controllers/user.go
+++ controllers/user.go
@@ -63,16 +63,13 @@ func (c *UserController) HandleRed(){
 	}
 
 	// 发送激活邮件
-
-
-
 	emailConfig := `{"username":"431592976@qq.com","password":"nabvchjshgkacbbb","host":"smtp.qq.com","port":587}`
 	emailConn := utils.NewEMail(emailConfig)
 	emailConn.From = "431592976@qq.com"  // 指定发件人的邮箱地址
 	emailConn.To = []string{user.Email}  // 指定收件人邮箱地址
 	emailConn.Subject = "鲜淘驿站用户激活"  // 指定邮件的标题
 	// 发给用户的是激活请求地址
-	emailConn.HTML= user.Name + "您好，<br>"+"感谢您注册鲜淘驿站，为了避免您忘记账号或密码导致您的账户无法找回，请您验证Email地址。<br><a href=127.0.0.1:8080/active?id=" + strconv.Itoa(user.Id) + ">点击验证</a><br>------------<br>按钮无效？请复制粘贴下面的链接至浏览器地址栏打开：<br>"+"127.0.0.1:8080/active?id=" + strconv.Itoa(user.Id)
+	emailConn.HTML= `尊敬的` + user.Name + `，您好<br><br>感谢您注册鲜淘驿站，为了避免您忘记账号或密码导致您的账户无法找回，请您验证Email地址。<br><br>请复制粘贴下面的链接至浏览器地址栏打开：<br><br>127.0.0.1:8080/active?id=` + strconv.Itoa(user.Id) + `<br>`
 	err = emailConn.Send()
 	if err != nil {
 		c.Data["errMsg"] = "发送激活邮件失败，请重新发送~"
@@ -80,15 +77,9 @@ func (c *UserController) HandleRed(){
 		return
 	}
 
-
 	// 返回视图
 	c.Ctx.WriteString("注册成功，请去邮箱激活用户。")
-
-
 	// c.Redirect("/login", 302)
-
-
-
 }
 // 激活用户
 func (c *UserController) ActiveUser(){
@@ -118,6 +109,17 @@ func (c *UserController) ActiveUser(){
 }
 // 显示登录页面
 func (c *UserController) ShowLogin(){
+	userName := c.Ctx.GetCookie("userName")
+	// base64解密
+	tempUserName, _ := base64.StdEncoding.DecodeString(userName)
+	if string(tempUserName) == "" {
+		// 没有记住用户名
+		c.Data["userName"] = ""
+		c.Data["checked"] = ""
+	}else{
+		c.Data["userName"] = string(tempUserName)
+		c.Data["checked"] = "checked"
+	}
 	c.TplName = "login.html"
 }
 // 处理登录数据
@@ -139,16 +141,19 @@ func (c *UserController) HandleLogin(){
 	err := o.Read(&user, "Name")
 	if err != nil {
 		c.Data["errMsg"] = "用户名或密码错误"
+		c.Data["userName"] = userName
 		c.TplName = "login.html"
 		return
 	}
 	if user.Password != password {
 		c.Data["errMsg"] = "用户名或密码错误"
+		c.Data["userName"] = userName
 		c.TplName = "login.html"
 		return
 	}
 	if user.Active != true {
 		c.Data["errMsg"] = "用户名未激活，请先前往邮箱激活"
+		c.Data["userName"] = userName
 		c.TplName = "login.html"
 		return
 	}
@@ -161,25 +166,11 @@ func (c *UserController) HandleLogin(){
 	} else {
 		c.Ctx.SetCookie("userName", userName, -1)
 	}
-
-
-
 	// 返回视图
 	c.Redirect("/", 302)
 }
 // 显示主页
 func (c *UserController) ShowIndex(){
-	userName := c.Ctx.GetCookie("userName")
-	// base64解密
-	tempUserName, _ := base64.StdEncoding.DecodeString(userName)
-	if string(tempUserName) == "" {
-		// 没有记住用户名
-		c.Data["userName"] = ""
-		c.Data["checked"] = ""
-	}else{
-		c.Data["userName"] = string(tempUserName)
-		c.Data["checked"] = "checked"
-	}
 	// 返回视图
 	c.TplName = "index.html"
 }
